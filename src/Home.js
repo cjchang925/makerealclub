15d6c75c4774c42c3df716b7043825ba09a5ca24
Chi-Chun Chang <cjchang925@gmail.com>
2022-09-04 15:03:21 +0800

    feat: Add loading page to home page

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,33 @@
 import ClubLogo from "./components/ClubLogo";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ClubIntro from "./components/ClubIntro";
 import './Home.css'
 import UnderCoverThreeIntros from "./components/UnderCover";
+import LoadingPage from './components/LoadingPage'
 
 const Home = () => {
-    return (
-        <div className='home'>
-            <div className="home_inside">
-                <ClubLogo />
-                <ClubIntro />
-                <UnderCoverThreeIntros />
+    const [loading, setLoading] = useState("true");
+
+    useEffect(() => {
+        setLoading("false");
+    }, []);
+
+    if (loading === "true") {
+        return (
+            <LoadingPage />
+        )
+    }
+    else {
+        return (
+            <div className='home'>
+                <div className="home_inside">
+                    <ClubLogo />
+                    <ClubIntro />
+                    <UnderCoverThreeIntros />
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 export default Home;
---
+++ src/components/LoadingPage.js
@@ -0,0 +1,15 @@
+import './LoadingPage.css'
+import PropagateLoader from "react-spinners/PropagateLoader";
+
+const LoadingPage = () => {
+    return (
+        <div className='loading_page'>
+            <h2 className='loading_title'>
+                歡迎來到交大創客！
+            </h2>
+            <PropagateLoader cssOverride={{}} color={'orange'} style={{position: 'absolute', top: '55vh' }}/>
+        </div>
+    )
+}
+
+export default LoadingPage;
