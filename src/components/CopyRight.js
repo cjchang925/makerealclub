a71d8a75442b970e1fc23a8ffe00f2c5b37f459e
Chi-Chun Chang <cjchang925@gmail.com>
2023-03-01 16:55:46 +0800

    fix: Modify the author names.

--- src/components/CopyRight.js
+++ src/components/CopyRight.js
@@ -6,7 +6,7 @@ const CopyRight = (props) => {
         <div className='copy_right'>
             <img src={copyrighticon} alt='icon' className='copy_right_icon' />
             <p className='copy_right_text' style={{color: props.color}}>
-                &nbsp;2023 NYCU Makereal Labs&nbsp;|&nbsp;Developed by <a href='https://github.com/cjchang925' rel="noreferrer" target="_blank">Chi-chun Chang</a>
+                &nbsp;2023 NYCU Makereal Labs&nbsp;|&nbsp;Developed by <a href='https://github.com/cjchang925' rel="noreferrer" target="_blank">Chi-chun Chang</a> and <a href='https://github.com/CaiCian' rel="noreferrer" target="_blank">Cai-cian Chuang</a>
             </p>
         </div>
     );
